c26fb6bbc32f4a97a86a7fe58f7180d7cacf7002
Vamilu Yovan Saputra <saputra.22.yovan@gmail.com>
2017-03-11 22:06:05 +0800

    posttest2

---
+++ laravel5/app/Http/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function() {
+	return view('welcome');
+});
+
+
+Route::get('hello-world', function () {
+    return 'Hello World';
+});
+
+
+Route::get('pengguna/{pengguna}', function ($pengguna) {
+    return "Hello-world dari $pengguna" ;
+});
+
+
+Route::get('kelas_b/framework/{mhs?}', function ($mhs="Anonim") {
+    return 'Selamat Datang $mhs';
+});
